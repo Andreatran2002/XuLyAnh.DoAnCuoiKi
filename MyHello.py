d5391735ab9ff338bd4e633744453b76e7851851
Tran Diep Phuong Vy <andreatran2002@gmail.com>
2023-05-01 23:49:46 +0700

    init project

---
+++ MyHello.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="tong_quan",
+    page_icon="👋",
+)
+
+st.write("# Báo cáo cuối kì ")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    ### Nội dung của bài báo cáo cuối kì:
+
+        1. Phát hiện khuôn mặt
+
+        2. Nhận dạng khuôn mặt của chính mình
+
+        3. Xử lý ảnh
+    
+    ### Thành viên nhóm
+        1. Trần Diệp Phương Vy . MSSV :20110232
+        2. Phạm Nhật Tiến . MSSV :20110735
+"""
+)
